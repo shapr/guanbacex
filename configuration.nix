0b1d2048bc42da295fba4d56a84f318f88d38dca
Shae Erisson <shae@scannedinavian.com>
2025-12-04 21:33:03 -0500

    aand now we native build all of NixOS for this AMD EPYC?

--- configuration.nix
+++ configuration.nix
@@ -6,11 +6,11 @@
 
 {
   # AMD EPYC 4564P ?
-  # nixpkgs.hostPlatform = {
-  #   gcc.arch = "znver4";
-  #   gcc.tune = "znver4";
-  #   system = "x86_64-linux";
-  # };
+  nixpkgs.hostPlatform = {
+    gcc.arch = "znver4";
+    gcc.tune = "znver4";
+    system = "x86_64-linux";
+  };
 
   nix = {
     # https://github.com/NixOS/nix/issues/11728#issuecomment-2613076734 for download-buffer-size
