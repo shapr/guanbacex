1445367cb55d9846ee29076980200a3dd4f4223a
Shae Erisson <shae+jj@scannedinavian.com>
2025-12-17 21:29:58 -0500

    set flake to nixos 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     emacs-overlay.url = "github:nix-community/emacs-overlay/master";
     emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
